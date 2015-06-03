cb92b9ef1c43200a5cc0188db6424de06e13e535
Elizabeth Sander <esander@frair>
2015-06-03 14:22:16 -0500

    merge fixes

--- R/load_scripts.R
+++
@@ -1,9 +0,0 @@
-loadModelScript <- function(){
-    source(system.file(package = 'predictimportance',
-                       'scripts/FullSimulationModels.R'))
-}
-
-loadEmpiricalScript <- function(){
-    source(system.file(package = 'predictimportance',
-                       'scripts/FullSimulationEmpirical.R'))
-}
